316ba7b6e418fd4730936f7c8f0655ea06afcad9
ahmed <ahmed.ibrahim@insa-rennes.fr>
2024-08-10 21:00:51 +0200

    Added Welcome testing message

--- App.js
+++ App.js
@@ -2,9 +2,10 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  const welcomeMsg = "Hello World! Testing Message...";
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text> {welcomeMsg} </Text>
       <StatusBar style="auto" />
     </View>
   );
