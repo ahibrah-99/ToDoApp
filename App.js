c4a8400c867508d41f8cac5a90cc28ff4c7da538
ahmed <ahmed.ibrahim@insa-rennes.fr>
2024-08-12 11:12:19 +0200

    Code layout moddified

--- App.js
+++ App.js
@@ -1,5 +1,8 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { 
+  StyleSheet,
+  Text, 
+  View } from 'react-native';
 
 export default function App() {
   const welcomeMsg = "Hello World! Testing Message...";
