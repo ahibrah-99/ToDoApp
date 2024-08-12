68179c145d4605de74bea56338425c71effacf18
ahmed <ahmed.ibrahim@insa-rennes.fr>
2024-08-12 11:25:01 +0200

    import task class

--- App.js
+++ App.js
@@ -1,8 +1,14 @@
+// Import expo status bar
 import { StatusBar } from 'expo-status-bar';
+
+// Import components
 import { 
   StyleSheet,
   Text, 
   View } from 'react-native';
+
+// Import task class
+import task from 'task.js';
 
 export default function App() {
   const welcomeMsg = "Hello World! Testing Message...";
