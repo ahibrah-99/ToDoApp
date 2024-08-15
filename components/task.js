4ccb8313eb5853ad4924f54152d177662c9e9eb2
ahmed <ahmed.ibrahim@insa-rennes.fr>
2024-08-15 10:23:23 +0200

    Changed location of task.js

--- components/task.js
+++ components/task.js
@@ -1,5 +1,5 @@
 // Task class definition
-export class task {
+export default class task {
     constructor(label) {
         this.label = label;
         this.status = 0;        // 0 for pending and 1 for done
