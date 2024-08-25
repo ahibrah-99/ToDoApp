8a3fff3cd52793852215b096848fb93f13879deb
ahmed <ahmed.ibrahim@insa-rennes.fr>
2024-08-25 18:20:38 +0300

    Apply initial Redux changes

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { StyleSheet, Text, View, TextInput, TouchableOpacity, FlatList , Image} from 'react-native';
 import Task from './src/components/Task';
 import TaskItem from './src/components/taskItem';
+import ToDoApp from './src/ToDoApp';
 
 /* Save and Load data using AsyncStorage API */
 import AsyncStorage from '@react-native-async-storage/async-storage'; //install using npm install @react-native-async-storage/async-storage
@@ -102,80 +103,6 @@ export default function App() {
   }
 
   return (
-  <View style={styles.container}>
-    <Image style={styles.Image} source={require("./imgs/ToDoIMG.png")}/>
-    <Text style={styles.heading}>My ToDo List</Text>
-    <TextInput
-      style={styles.input}
-      placeholder="Enter task"
-      value={task}
-      onChangeText={(text) => setTaskLabel(text)}
-    />
-    <TouchableOpacity
-      style={styles.addButton}
-      onPress={addTask}>
-    <Text style={styles.addButtonText}>
-        {editIndex !== -1 ? "Update Task" : "Add Task"}
-    </Text>
-    </TouchableOpacity>
-      <FlatList
-        data={tasks}
-        renderItem={({ item, index }) => (
-        <TaskItem
-            item={item}
-            index={index}
-            markTaskDone={markTaskDone}
-            editTask={editTask}
-            deleteTask={deleteTask}
-            isEditing={isEditing}  // Pass isEditing to TaskItem
-        />
-        )}
-      keyExtractor={(item, index) => index.toString()}
-      />
-  </View>
+    <ToDoApp />
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-  flex: 1,
-  padding: 40,
-  marginTop: 20,
-  backgroundColor: '#fff',
-  alignItems: 'center',
-  justifyContent: 'center',
-  alignSelf: 'stretch',
-  },
-  Image: {
-    padding: 20,
-    width: 80,
-    height: 100,
-  },
-  heading: {
-  fontSize: 30,
-  fontWeight: "bold",
-  marginBottom: 7,
-  color: "black",
-  },
-  input: {
-  borderWidth: 3,
-  borderColor: "#ccc",
-  padding: 10,
-  marginBottom: 10,
-  borderRadius: 10,
-  fontSize: 18,
-  width: '70%'
-  },
-  addButton: {
-  backgroundColor: "black",
-  padding: 10,
-  borderRadius: 5,
-  marginBottom: 10,
-  },
-  addButtonText: {
-  color: "white",
-  fontWeight: "bold",
-  textAlign: "center",
-  fontSize: 18,
-  },
-});
---
+++ src/ToDoApp.js
@@ -0,0 +1,90 @@
+import React, { Component } from "react";
+import { StyleSheet, Text, View, TextInput, TouchableOpacity, FlatList , Image} from 'react-native';
+
+
+class ToDoApp extends Component {
+    render() {
+        return (
+            <View style={styles.container}>
+                <Image style={styles.Image} source={require("../imgs/ToDoIMG.png")}/>
+                <Text style={styles.heading}>My ToDo List</Text>
+
+            </View>
+        );
+    }
+}
+export default ToDoApp;
+
+const styles = StyleSheet.create({
+    container: {
+    flex: 1,
+    padding: 40,
+    marginTop: 20,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+    alignSelf: 'stretch',
+    },
+    Image: {
+      padding: 20,
+      width: 80,
+      height: 100,
+    },
+    heading: {
+    fontSize: 30,
+    fontWeight: "bold",
+    marginBottom: 7,
+    color: "black",
+    },
+    input: {
+    borderWidth: 3,
+    borderColor: "#ccc",
+    padding: 10,
+    marginBottom: 10,
+    borderRadius: 10,
+    fontSize: 18,
+    width: '70%'
+    },
+    addButton: {
+    backgroundColor: "black",
+    padding: 10,
+    borderRadius: 5,
+    marginBottom: 10,
+    },
+    addButtonText: {
+    color: "white",
+    fontWeight: "bold",
+    textAlign: "center",
+    fontSize: 18,
+    },
+  });
+
+  /*
+                <TextInput
+                    style={styles.input}
+                    placeholder="Enter task"
+                    value={task}
+                    onChangeText={(text) => setTaskLabel(text)}
+                />
+                <TouchableOpacity
+                    style={styles.addButton}
+                    onPress={addTask}>
+                <Text style={styles.addButtonText}>
+                    {editIndex !== -1 ? "Update Task" : "Add Task"}
+                </Text>
+                </TouchableOpacity>
+                    <FlatList
+                        data={tasks}
+                        renderItem={({ item, index }) => (
+                        <TaskItem
+                            item={item}
+                            index={index}
+                            markTaskDone={markTaskDone}
+                            editTask={editTask}
+                            deleteTask={deleteTask}
+                            isEditing={isEditing}  // Pass isEditing to TaskItem
+                        />
+                )}
+              keyExtractor={(item, index) => index.toString()}
+              />
+  */
